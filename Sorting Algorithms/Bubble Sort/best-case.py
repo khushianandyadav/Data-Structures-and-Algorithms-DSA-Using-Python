89a5e1f7e89ff7e302113f405ff80d9e1de9fcc6
khushianandyadav <khushiy1904@gmail.com>
2026-01-13 12:14:32 +0530

    Add files via upload

---
+++ Sorting Algorithms/Bubble Sort/best-case.py
@@ -0,0 +1,17 @@
+nums = [1, 2, 4, 5, 9, 10, 12, 14]
+
+n = len(nums)
+for i in range(n-2, -1, -1):
+    is_swap = False
+    for j in range(0, i+1):
+        if nums[j] > nums[j+1]:
+            nums[j], nums[j+1] = nums[j+1], nums[j]
+            is_swap = True
+    if is_swap == False:
+        break
+
+print(nums)
+
+
+# TC -> O(N) ____ only for best case
+# SC -> O(1)
