b24a30650f84c1ed289fc08ef52fc408f9b5bfe1
khushianandyadav <khushiy1904@gmail.com>
2026-01-18 18:38:07 +0530

    Update method1a.py

--- List Problems(Easy)/Largest Element/method1a.py
+++ List Problems(Easy)/Largest Element/method1a.py
@@ -1,6 +1,6 @@
-nums = [55, 135, 32, -97, 99, 3, 67]
+nums = [55, 32, -97, 99, 3, 67]
 
-largest = float("-inf")
+largest = nums[0]
 n = len(nums)
 for i in range(0, n):
     largest = max(largest, nums[i])
