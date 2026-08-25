24ba86625801f5af01d92e17a7db49331c9bea22
khushianandyadav <khushiy1904@gmail.com>
2026-08-25 21:49:37 +0530

    Add files via upload

---
+++ List Problems(Hard)/Binary Search/Find Minimum in Rotated Sorted Array/optimal_solution.py
@@ -0,0 +1,17 @@
+nums = [7, 8, 9, 1, 2, 3, 4]
+n = len(nums)
+low = 0
+high = n-1
+mini = float("inf")
+while low <= high:
+    mid = (low + high) // 2
+    if nums[mid] <= nums[high]:
+        mini = min(mini, nums[mid])
+        high = mid-1
+    else:
+        mini = min(mini, nums[low])
+        low = mid+1
+print(mini)
+
+# TC -> O(log N)
+# SC -> O(1)
